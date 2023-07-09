3280217b25132223db0395d1859cbe2e60376664
Bani Faza Rasyadan <banifr@Banis-MacBook-Pro.local>
2023-07-09 22:14:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ccdp_10119102_mvp/MainView.java
@@ -0,0 +1,7 @@
+package com.example.ccdp_10119102_mvp;
+
+public interface MainView {
+    void showKilometer(String kilometer);
+
+    void showCentimeter(String centimeter);
+}
---
+++ app/src/main/java/com/example/ccdp_10119102_mvp/Meter.java
@@ -0,0 +1,19 @@
+package com.example.ccdp_10119102_mvp;
+
+public class Meter {
+    private double meter;
+
+    public void setMeter(double meter) {
+        this.meter = meter;
+    }
+
+    public double toKilometer() {
+        double kilometer =  meter / 1000;
+        return kilometer;
+    }
+
+    public double toCentimeter() {
+        double centimeter =  meter * 100;
+        return centimeter;
+    }
+}
